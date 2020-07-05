1705034466a6cca8b34a053493041653cdd22b6f
master-of-programming <therealprogrammerboy@protonmail.com>
2020-07-05 16:36:51 +0300

    Project Complated !

---
+++ app/src/main/res/drawable/app/src/main/java/com/example/web_browser/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.web_browser;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
